46110e6698249cdea295baba7f523f0746c0ba99
Angel Valencia <angelrvo@hotmail.com>
2024-04-29 11:43:21 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -2,6 +2,6 @@
 #SBATCH --time=00:15:00
 #SBATCH -N 1
 
-cp -r $HOME/slurm-bpai-tutorial/python $Angel
-cd $Angel/python
-python script.py
+cp -r $HOME/slurm-bpai-tutorial/python $TMPDIR
+cd $TMPDIR/python
+python Trial.py
