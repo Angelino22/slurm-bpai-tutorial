5f3c30927d574c0139f507ceeb6c680955cae0a8
Angel Valencia <angelrvo@hotmail.com>
2024-06-12 21:41:46 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -5,22 +5,21 @@
 #SBATCH --array=1-5
 
 mkdir -p $TMPDIR/ava940
-cp -r $HOME/var/scratch/ava940 $TMPDIR/ava940
+cp -r /var/scratch/ava940 $TMPDIR/ava940
 cd $TMPDIR/ava940/DQN/cleanrlangel
 module load gnu9/9.4.0
 conda init
 conda activate bpaiAngel
 seeds=(123 456 789 101112 131415)
-seed=${seeds[$SLURM_ARRAY_TASK_ID - 1]} 
+seed=${seeds[$SLURM_ARRAY_TASK_ID - 1]}
 python cleanrl/Experiments.py \
     --seed $seed \
     --env-id Foozpong_v3 \
-    --total-timesteps 50000
-    --track 
-    --wandb_project_name Foozy
+    --total-timesteps 50000 \
+    --track \
+    --wandb_project_name Foozy \
     --capture_video
 rm -rf $TMPDIR/ava940/
-
 
 
 
