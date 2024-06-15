69f9807587844ee015d03a6c3540504a9db90bdd
Angel Valencia <angelrvo@hotmail.com>
2024-06-15 23:24:45 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -17,9 +17,9 @@ seed=${seeds[$SLURM_ARRAY_TASK_ID - 1]}
 python Experiments \
     --seed $seed \
     --env-id Foozpong_v3 \
-    --total-timesteps 1000000 \
+    --total-timesteps 5000000 \
     --track \
-    --wandb_project_name FINALPONG_2 \
+    --wandb_project_name UNTRAINED \
     --wandb_entity Angelrvo2002\
     --capture_video
 rm -rf $TMPDIR/ava940/
