ae16d39164efb24e849502b49c07c305baf75cc9
Angel Valencia <angelrvo@hotmail.com>
2024-06-15 12:26:27 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=cleanrltest
-#SBATCH --time=07:00:00
+#SBATCH --time=00:15:00
 #SBATCH -N 1
 #SBATCH --array=1-5
 #SBATCH --mail-user=angelrvo@hotmail.com
-#SBATCH --mail-type ALL
+#SBATCH --mail-type=ALL
 
-# Ensure the directory exists and is empty
+
 mkdir -p $TMPDIR/ava940
-
-# Copy all necessary files excluding .git and anaconda3 directories
 rsync -av --exclude='.git' --exclude='anaconda3' /var/scratch/ava940/ $TMPDIR/ava940/
-
-# Navigate to the directory containing the Python script
 cd $TMPDIR/ava940/DQN/cleanrlangel/cleanrl || exit 1
-
-# Debugging output to ensure we are in the correct directory
-pwd
-ls -l
-
-# Set environment and execute the Python script
 module load gnu9/9.4.0
 conda init
 conda activate bpaiAngel
@@ -28,11 +18,9 @@ seed=${seeds[$SLURM_ARRAY_TASK_ID - 1]}
 python Experiments \
     --seed $seed \
     --env-id Foozpong_v3 \
-    --total-timesteps 4000000 \
+    --total-timesteps 10000 \
     --track \
-    --wandb_project_name FINALPONG \
+    --wandb_project_name FINALPONG_2 \
     --wandb_entity Angelrvo2002\
     --capture_video
-
-# Cleanup: Remove the copied directory after the experiment
 rm -rf $TMPDIR/ava940/
